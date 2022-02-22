b9321beab9f762571f662ef3c0c65c619329e540
TralahM <briantralah@gmail.com>
2022-02-22 21:44:57 +0300

    currency test

--- pkg/currency/currency_test.go
+++ pkg/currency/currency_test.go
@@ -8,11 +8,11 @@ func TestCurrency(t *testing.T) {
 	t.Run("Test Prints.", func(t *testing.T) {
 		var b Bitcoin
 		b = 289
-		t.Error(b.Code())
-		t.Error(b.Country())
-		t.Error(b.Symbol())
-		t.Error(b.Name())
-		t.Error(b.ExchangeRate(b))
-		t.Error(b)
+		t.Log(b.Code())
+		t.Log(b.Country())
+		t.Log(b.Symbol())
+		t.Log(b.Name())
+		t.Log(b.ExchangeRate(b))
+		t.Log(b)
 	})
 }
